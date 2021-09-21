696546de3575c84aada5eb25bb5059fe12644e15
Randy <randy.godwin@natcheztr.com>
2021-09-21 15:44:31 +0300

    add in the tf -variable file
    Former-commit-id: 292b40a6a8d7c684f89dd1f41981a77693cc33bc

--- main.tf
+++ main.tf
@@ -45,3 +45,35 @@ resource "aws_default_route_table" "main-rtb" {
         Name = "${var.env_prefix}-main-rtb"
     }
 }
+
+
+resource "aws_security_group" "myapp-sg" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    name = "myapp-sg"
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [var.my_ip]
+    }
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = []
+    }
+
+    tags = {
+        Name = "${var.env_prefix}-sg"
+    }
+}
