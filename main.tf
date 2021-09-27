ecbb39cb8ad80001f71f6fc05f790e57e4f3a177
rbgodwin <randy.godwin@natcheztr.com>
2021-09-27 11:54:26 +0300

    fix type
    Former-commit-id: 960099d85b2543d695b1306785f1e3d1aac17252

--- main.tf
+++ main.tf
@@ -96,7 +96,7 @@ output "aws_ami_id" {
 }
 
 output "ec2_public_ip" {
-    value = aws_instance.myapp-server.ec2_public_ip
+    value = aws_instance.myapp-server.public_ip
 }
 
 resource "aws_key_pair" "ssh-key"{
