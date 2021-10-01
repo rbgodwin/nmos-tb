9fe3e99c9a784cf746b41c8fbd19bd12a056288f
Randy Godwin <rbgodwin@natcheztr.com>
2021-10-01 10:28:44 +0300

    Works with NMOS related docker RDS container
    Former-commit-id: 4b1df0d841fe2c64e8a20abc0debaaf99484dd95

--- entry-script.sh
+++ entry-script.sh
@@ -4,3 +4,5 @@ sudo yum install -y docker
 sudo systemctl start docker
 sudo usermod -aG docker ec2-user
 docker run -p 8080:80 nginx
+docker pull rhastie/nmos-cpp:latest
+docker run -it --net=host --privileged --rm rhastie/nmos-cpp:latest
--- main.tf
+++ main.tf
@@ -65,6 +65,14 @@ resource "aws_default_security_group" "default-sg" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+# NMOS RDS
+     ingress {
+        from_port = 8010
+        to_port = 8010
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     egress {
         from_port = 0
         to_port = 0
