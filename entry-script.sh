a027ac262dc5df382c37bd3ce68a6f6c286845ce
Randy Godwin <rbgodwin@natcheztr.com>
2021-10-05 13:57:01 +0300

    Checkpoint
    Former-commit-id: 237241c6e67bad0a91b7e081f2ac4ce8f377f113

--- entry-script.sh
+++ entry-script.sh
@@ -3,10 +3,10 @@ sudo yum update -y
 sudo yum install -y docker
 sudo systemctl start docker
 sudo usermod -aG docker ec2-user
-docker run -d -p 8080:80 nginx 
+sudo docker run -d -p 8080:80 nginx 
 
 # Crank up a BIND9 DNS Server for our DNS-SD Needs
-docker run -d \
+sudo docker run -d \
         --name=bind9 \
         --restart=always \
         --publish 53:53/udp \
@@ -18,5 +18,5 @@ docker run -d \
         --volume /var/log \
         internetsystemsconsortium/bind9:9.16
 docker pull rhastie/nmos-cpp:latest
-docker run -d --net=host --privileged --rm rhastie/nmos-cpp:latest 
-docker run --env RUN_NODE=TRUE -d --net=host --privileged --rm rhastie/nmos-cpp:latest
+sudo docker run -d --net=host --privileged --rm rhastie/nmos-cpp:latest 
+sudo docker run --env RUN_NODE=TRUE -d --net=host --privileged --rm rhastie/nmos-cpp:latest
