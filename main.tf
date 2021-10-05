4a5bd36b08e46ef93f504e6b644c8454fa4df423
Randy Godwin <rbgodwin@natcheztr.com>
2021-10-05 16:01:37 +0300

    move to modulular structure
    Former-commit-id: eed4528eda8ea7e746904b15c69b2dfdeacdcad7

--- main.tf
+++ main.tf
@@ -2,16 +2,6 @@ provider "aws" {
     region = "eu-west-1"
 }
 
-variable vpc_cidr_block {}
-variable subnet_cidr_block {}
-variable avail_zone {}
-variable env_prefix {}
-variable my_ip {}
-variable instance_type {}
-variable public_key_location {}
-variable private_key_location {}
-
-
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
     tags = {
@@ -19,34 +9,14 @@ resource "aws_vpc" "myapp-vpc" {
     }
 }
 
-resource "aws_subnet" "myapp-subnet-1" {
+module "myapp-subnet" {
+    source = "./modules/subnet"
+    subnet_cidr_block = var.subnet_cidr_block
+    avail_zone = var.avail_zone 
+    env_prefix = var.env_prefix
     vpc_id = aws_vpc.myapp-vpc.id
-    cidr_block = var.subnet_cidr_block
-    availability_zone = var.avail_zone
-    tags = {
-        Name = "${var.env_prefix}-subnet-1"
-    }
-}
-
-resource "aws_internet_gateway" "myapp-igw" {
-    vpc_id = aws_vpc.myapp-vpc.id
-    tags = {
-        Name = "${var.env_prefix}-igw"
-    }
-}
-
-resource "aws_default_route_table" "main-rtb" {
     default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
-
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.myapp-igw.id
-    }
-    tags = {
-        Name = "${var.env_prefix}-main-rtb"
-    }
 }
-
 
 resource "aws_default_security_group" "default-sg" {
     vpc_id = aws_vpc.myapp-vpc.id
@@ -99,13 +69,6 @@ data "aws_ami" "latest-amazon-linux-image" {
     }
 }
 
-output "aws_ami_id" {
-    value = data.aws_ami.latest-amazon-linux-image.id
-}
-
-output "ec2_public_ip" {
-    value = aws_instance.myapp-server.public_ip
-}
 
 resource "aws_key_pair" "ssh-key"{
     key_name = "server-key-work"
@@ -116,7 +79,8 @@ resource "aws_instance" "myapp-server" {
      ami = data.aws_ami.latest-amazon-linux-image.id
      instance_type = var.instance_type
 
-     subnet_id = aws_subnet.myapp-subnet-1.id
+     subnet_id = module.myapp-subnet.subnet.id
+
      vpc_security_group_ids = [aws_default_security_group.default-sg.id]
      availability_zone = var.avail_zone
      
