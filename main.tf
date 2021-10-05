892690cc58b5c6a98fb4d957a16c874d1103078c
Randy Godwin <rbgodwin@natcheztr.com>
2021-10-05 16:33:58 +0300

    move to modulular structure
    Former-commit-id: 0d4c29a1bccf855d6705c60b72e4b486304b597e

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ resource "aws_vpc" "myapp-vpc" {
     }
 }
 
+# Set up the VPC Network
 module "myapp-subnet" {
     source = "./modules/subnet"
     subnet_cidr_block = var.subnet_cidr_block
@@ -18,99 +19,15 @@ module "myapp-subnet" {
     default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 }
 
-resource "aws_default_security_group" "default-sg" {
+# Set up the Application Server
+module "myapp-server" {
+    source = "./modules/webserver"
     vpc_id = aws_vpc.myapp-vpc.id
-
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = [var.my_ip]
-    }
-
-    ingress {
-        from_port = 8080
-        to_port = 8080
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-# NMOS RDS
-     ingress {
-        from_port = 8010
-        to_port = 8010
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-        prefix_list_ids = []
-    }
-
-    tags = {
-        Name = "${var.env_prefix}-sg"
-    }
+    my_ip = var.my_ip
+    env_prefix = var.env_prefix
+    image_name = var.image_name
+    public_key_location = var.public_key_location
+    instance_type = var.instance_type
+    subnet_id = module.myapp-subnet.subnet.id
+    avail_zone = var.avail_zone
 }
-
-data "aws_ami" "latest-amazon-linux-image" {
-    most_recent = true
-    owners = ["amazon"]
-    filter {
-        name = "name"
-        values = ["amzn2-ami-hvm-*-gp2"]
-    }
-    filter {
-        name = "virtualization-type"
-        values = ["hvm"]
-    }
-}
-
-
-resource "aws_key_pair" "ssh-key"{
-    key_name = "server-key-work"
-    public_key = file(var.public_key_location)
-}
-
-resource "aws_instance" "myapp-server" {
-     ami = data.aws_ami.latest-amazon-linux-image.id
-     instance_type = var.instance_type
-
-     subnet_id = module.myapp-subnet.subnet.id
-
-     vpc_security_group_ids = [aws_default_security_group.default-sg.id]
-     availability_zone = var.avail_zone
-     
-     associate_public_ip_address = true
-     key_name = "server-key-work"
-
-    user_data = file("entry-script.sh")
-
-    connection {
-        type ="ssh"
-        host = self.public_ip
-        user = "ec2-user"
-        private_key = file(var.private_key_location)
-    }
-
-    provisioner "file" {
-        source = "entry-script.sh"
-        destination = "/home/ec2-user/entry-script.sh"
-    }
-
-    provisioner "remote-exec" {
-        inline = [
-            "chmod +x /home/ec2-user/entry-script.sh",
-            "/home/ec2-user/entry-script.sh"
-        ]
-    }
-
-     tags = {
-        Name = "${var.env_prefix}-server"
-    }     
-
- }
-
---
+++ modules/webserver/main.tf
@@ -0,0 +1,99 @@
+
+
+#  Security Group 
+resource "aws_default_security_group" "default-sg" {
+    vpc_id = var.vpc_id
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [var.my_ip]
+    }
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+# NMOS RDS
+     ingress {
+        from_port = 8010
+        to_port = 8010
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = []
+    }
+
+    tags = {
+        Name = "${var.env_prefix}-sg"
+    }
+}
+
+data "aws_ami" "latest-amazon-linux-image" {
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name = "name"
+        values = [var.image_name]
+    }
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+
+resource "aws_key_pair" "ssh-key"{
+    key_name = "server-key-work"
+    public_key = file(var.public_key_location)
+}
+
+resource "aws_instance" "myapp-server" {
+     ami = data.aws_ami.latest-amazon-linux-image.id
+     instance_type = var.instance_type
+
+     subnet_id = var.subnet_id
+
+     vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+     availability_zone = var.avail_zone
+     
+     associate_public_ip_address = true
+     key_name =  aws_key_pair.ssh-key.key_name
+
+#    user_data = file("entry-script.sh")
+
+    connection {
+        type ="ssh"
+        host = self.public_ip
+        user = "ec2-user"
+        private_key = file(var.private_key_location)
+    }
+
+    provisioner "file" {
+        source = "entry-script.sh"
+        destination = "/home/ec2-user/entry-script.sh"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "chmod +x /home/ec2-user/entry-script.sh",
+            "/home/ec2-user/entry-script.sh"
+        ]
+    }
+
+     tags = {
+        Name = "${var.env_prefix}-server"
+    }     
+
+ }
+
