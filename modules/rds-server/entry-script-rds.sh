31adaf242720057fa9fa06e5ce79090537cec162
rbgodwin <randy.godwin@macnica.com>
2021-11-22 04:10:07 -0600

    Add in restart for docker containters
    Former-commit-id: e1d6fa2d67645df81e066393c1df3387b1966269

--- modules/rds-server/entry-script-rds.sh
+++ modules/rds-server/entry-script-rds.sh
@@ -8,5 +8,5 @@ sudo usermod -aG docker ec2-user
 
 
 sudo docker pull rhastie/nmos-cpp:latest
-sudo docker run -d -v /tmp/container-config:/home/container-config -v /tmp:/tmp --name RDS --net=host --privileged --rm rhastie/nmos-cpp:latest  
-sudo docker run -d -v /tmp/container-config:/home/container-config -v /tmp:/tmp --name Node --env RUN_NODE=TRUE --net=host --privileged --rm rhastie/nmos-cpp:latest  
+sudo docker run -d --restart unless-stopped -v /tmp/container-config:/home/container-config -v /tmp:/tmp --name RDS --net=host --privileged rhastie/nmos-cpp:latest  
+sudo docker run -d --restart unless-stopped -v /tmp/container-config:/home/container-config -v /tmp:/tmp --name Node --env RUN_NODE=TRUE --net=host --privileged rhastie/nmos-cpp:latest  
