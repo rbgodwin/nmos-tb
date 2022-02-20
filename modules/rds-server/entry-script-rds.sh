0d403f20f8c5acd044ad0f467ad1f49cb0cc88a9
rbgodwin <randy.godwin@natcheztr.com>
2022-02-20 17:26:48 +0300

    port map the Broadcast controller
    Former-commit-id: 8e6fa41a441904a66b88fa0dec0579c84eb80e8d

--- modules/rds-server/entry-script-rds.sh
+++ modules/rds-server/entry-script-rds.sh
@@ -10,12 +10,12 @@ sudo usermod -aG docker ec2-user
 sudo iptables -I DOCKER-USER -i eth0 -o docker0 -j ACCEPT
 # Easy-nmos docker containers
 sudo docker pull rhastie/nmos-cpp:latest
-sudo docker run -d --restart unless-stopped -v /tmp/container-config:/home/container-config -v /tmp:/tmp --name RDS  rhastie/nmos-cpp:latest  
+sudo docker run -d --restart unless-stopped -p 8010:8010 -p 8011:8011 -v /tmp/container-config:/home/container-config -v /tmp:/tmp --name RDS  rhastie/nmos-cpp:latest  
 sudo docker run -d --restart unless-stopped -v /tmp/container-config:/home/container-config -v /tmp:/tmp --name Node --env RUN_NODE=TRUE rhastie/nmos-cpp:latest  
 
 #AMWA Auto-Tests
 sudo docker pull amwa/nmos-testing
-sudo docker run -d --restart unless-stopped --v="/tmp/UserConfig.py:/config/UserConfig.py" --name NMOS-TESTING amwa/nmos-testing
+sudo docker run -d --restart unless-stopped -p 5000:5000 -p 5001:5001 --v="/tmp/UserConfig.py:/config/UserConfig.py" --name NMOS-TESTING amwa/nmos-testing
 
 # Go through our off-cloud gateways (10.0.59.0/24 via wireguard server) 
 sudo ip route add 10.0.59.0/24 via 10.0.50.59
